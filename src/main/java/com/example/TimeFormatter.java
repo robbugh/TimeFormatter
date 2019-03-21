ebd98d706607d3729c402bcf53ab9ba3b9cb81ca
Rob Bugh <robbugh@gmail.com>
2019-03-21 01:56:05 -0500

    Initial Drop

---
+++ src/main/java/com/example/TimeFormatter.java
@@ -0,0 +1,85 @@
+package com.example;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class can parse a time string into a numerical value, add minutes to the parsed time value and format
+ * the time value back into a string. The format of the time value must match the pattern, "[H]H:MM {AM|PM}". The
+ * output format matches this pattern as well.
+ */
+public class TimeFormatter {
+
+    private static String TIME_FORMAT = "^(\\d?\\d):(\\d\\d) (AM|PM)$";
+    private static Pattern PATTERN = Pattern.compile(TIME_FORMAT);
+    private static int ONE_DAY_IN_MINUTES = 1440;
+
+    private int totalMinutes;
+
+    /**
+     * Parses the specified time string into a numeric value representing the minutes in a day. The time string
+     * must match the pattern, "[H]H:MM {AM|PM}".
+     *
+     * @param timeStr Time string to parse
+     */
+    public TimeFormatter(String timeStr) throws Exception {
+        Matcher matcher = PATTERN.matcher(timeStr);
+        if (matcher.find()) {
+            int hours = Integer.parseInt(matcher.group(1));
+            if (hours < 0 || hours > 12) {
+                throw new Exception("Invalid Time String. Hours must be: 0 <= Hours <= 12");
+            }
+            int minutes = Integer.parseInt(matcher.group(2));
+            if (minutes >= 60 || minutes < 0 ) {
+                throw new Exception("Invalid Time String. Minutes must be: 0 <= minutes < 60");
+            }
+            boolean isAM = matcher.group(3).equals("AM");
+            if (isAM && hours == 12) {
+                hours = 0;
+            }
+            if (!isAM) {
+                hours += 12;
+            }
+            totalMinutes = (hours * 60) + minutes;
+        } else {
+            throw new Exception("Invalid Time String Format");
+        }
+    }
+
+    /**
+     * Adds a number of minutes to add to the total minutes this object represents. The additionalMinutes can be
+     * positive or negative.
+     *
+     * @param additionalMinutes The number of minutes to add to this object
+     * @return This object is returned to allow for method chaining
+     */
+    public TimeFormatter addMinutes(int additionalMinutes) {
+        totalMinutes += additionalMinutes;
+        if (totalMinutes < 0) {
+            totalMinutes += ONE_DAY_IN_MINUTES;
+        }
+        return this;
+    }
+
+    /**
+     * Formats the total minutes this object represents into a string matching the pattern, "[H]H:MM {AM|PM}".
+     * This format is the same as the input pattern used in the constructor.
+     *
+     * @return Formatted string using pattern, "[H]H:MM {AM|PM}".
+     */
+    public String format() {
+        String amPm = "AM";
+        int hours = (totalMinutes / 60) % 24;
+        if (hours >= 12) {
+            amPm = "PM";
+            if (hours > 12) {
+                hours -= 12;
+            }
+        }
+        if (hours == 0) {
+            hours = 12;
+        }
+        int minutes = totalMinutes % 60;
+        return String.format("%d:%02d %s", hours, minutes, amPm);
+    }
+}
---
+++ src/test/groovy/com/example/TimeFormatterSpec.groovy
@@ -0,0 +1,102 @@
+package com.example
+
+import spock.lang.Specification
+
+class TimeFormatterSpec extends Specification {
+
+    def "test the constructor"() {
+        when:
+        new TimeFormatter("")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter(":00 AM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("00: AM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("00:00")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("XX:00:AM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("00:XX AM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("00:00 XX")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("13:00 AM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("13:00 PM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("-12:00 AM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("12:60 PM")
+
+        then:
+        thrown Exception
+
+        when:
+        new TimeFormatter("12:-01 PM")
+
+        then:
+        thrown Exception
+    }
+
+    def "test the methods"() {
+        expect:
+        new TimeFormatter("1:00 AM").addMinutes(1440).format() == "1:00 AM"
+        new TimeFormatter("1:00 AM").addMinutes(-1440).format() == "1:00 AM"
+        new TimeFormatter("0:00 AM").addMinutes(0).format() == "12:00 AM"
+        new TimeFormatter("0:00 AM").addMinutes(1).format() == "12:01 AM"
+        new TimeFormatter("0:00 AM").addMinutes(-1).format() == "11:59 PM"
+        new TimeFormatter("12:00 AM").addMinutes(0).format() == "12:00 AM"
+        new TimeFormatter("12:00 AM").addMinutes(1).format() == "12:01 AM"
+        new TimeFormatter("12:00 AM").addMinutes(-1).format() == "11:59 PM"
+        new TimeFormatter("11:59 AM").addMinutes(0).format() == "11:59 AM"
+        new TimeFormatter("11:59 AM").addMinutes(1).format() == "12:00 PM"
+    }
+}
+
+
+
+
+
+
+
+
